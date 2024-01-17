ad027356588b66340ba0f7f2a64dc264cf639e2f
lucaspagliarin <lucas.pagliarin@gmail.com>
2024-01-17 13:57:36 -0300

    SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -9,8 +10,25 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>   
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 50,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/04/2022</td>   
+            </tr>
             <tr>
               <td width="50%">Desenvolvimento de site</td>
               <td>
